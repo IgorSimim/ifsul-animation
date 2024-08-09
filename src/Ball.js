a1e51adbb79ef41225b0c9f4b95d17b31e819e78
Igor Leon <igorleonsimim@gmail.com>
2024-08-09 02:23:14 -0300

    :sparkles: Add the ball class

---
+++ src/Ball.js
@@ -0,0 +1,33 @@
+import Circle from "./geometries/Circle";
+import { loadImage } from "./loaderAssets";
+
+export default class Ball extends Circle {
+	constructor(x, y, size, speed = 10, imgUrl) {
+		super(x, y, size, -(speed));
+		this.imgUrl = imgUrl;
+
+		loadImage(this.imgUrl).then(img => {
+			this.img = img;
+		});
+	}
+
+	draw(ctx) {
+		if (this.img) {
+			ctx.drawImage(this.img, this.x - this.size, this.y - this.size, this.size * 2, this.size * 2);
+		} else {
+			super.draw(ctx);
+		}
+	}
+
+	move(limits) {
+		this.x += this.speed;
+		this.limits(limits);
+	}
+
+	limits(limits) {
+		if (this.x + this.size < 0) {
+			this.x = limits.height + this.size;
+			this.y = Math.random() * limits.width;
+		}
+	}
+}
